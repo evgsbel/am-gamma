fee765abc5074aad8c47621c4dfad3ae2016a779
evgsbel <evgsbel@gmail.com>
2021-12-08 01:07:15 +0400

    finish case slider

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,37 +1,84 @@
 "use strict";
 
-// $(document).ready(function () {
-//   $('body').autoPadding({
-//     source: $('.js-header'),
-//   });
-//   //removeIf(production)
-//   console.log("document ready");
-//   //endRemoveIf(production)
-// });
-// fixed header
-$(document).ready(function () {
-  function stickySidebar() {
-    var scrollDistance = $(document).scrollTop(),
-        headerHeight = $('.header').outerHeight(true),
-        // sidebarHeight = $('aside').outerHeight(true),
-    footerOffsetTop = $('.js-stop-header').offset().top,
-        $header = $('header');
+//back to top
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
 
-    if (scrollDistance >= headerHeight) {
-      $header.addClass('header_fixed');
-      $header.removeClass('header_hide');
-    } else {
-      $header.removeClass('header_fixed');
-    }
-
-    if (scrollDistance + headerHeight >= footerOffsetTop - 100) {
-      $header.removeClass('header_fixed');
-      $header.addClass('header_hide');
-    }
+  if (height > 100) {
+    $('.js-to-top').fadeIn();
+  } else {
+    $('.js-to-top').fadeOut();
   }
-
-  stickySidebar();
-  $(document).scroll(function () {
-    stickySidebar();
-  });
 });
+$(document).ready(function () {
+  $(".js-to-top").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  });
+}); //openHideMenu
+
+$(function () {
+  var body = document.querySelector('body');
+  var openElem = document.querySelector('.js-open-hide-menu');
+  var hideMenu = document.querySelector('.hide-menu');
+  var sidebar = document.querySelector('.sidebar');
+  openElem.addEventListener('click', function (event) {
+    event.preventDefault();
+    openElem.classList.toggle('is-active');
+    hideMenu.classList.toggle('is-open');
+    sidebar.classList.toggle('opened-menu');
+    body.classList.toggle('opened-menu');
+  });
+}); // sub menu
+
+var openSubMenu = function openSubMenu() {
+  var panelHeading = document.querySelectorAll('.hide-menu__sub'),
+      panelContent = document.querySelector('.sub-menu');
+  panelHeading.forEach(function (elem) {
+    elem.addEventListener('click', function (event) {
+      event.preventDefault();
+      panelHeading.forEach(function (panelHeading) {
+        panelHeading.classList.remove('is-active');
+      });
+      var target = event.target;
+      elem.classList.add('is-active');
+    });
+  });
+};
+
+openSubMenu(); // case slider
+
+var sliderParent = document.getElementsByClassName("case");
+
+for (var i = 0; i < sliderParent.length; i++) {
+  var el = sliderParent[i];
+  var swiper = el.getElementsByClassName("js-case-swiper")[0];
+  var nx = el.getElementsByClassName("slider-navigation_next")[0];
+  var pr = el.getElementsByClassName("slider-navigation_prev")[0];
+  new Swiper(swiper, {
+    speed: 1000,
+    spaceBetween: 75,
+    loop: true,
+    grabCursor: true,
+    // If we need pagination
+    pagination: {
+      el: '.slider-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: nx,
+      prevEl: pr
+    },
+    on: {
+      slideChangeTransitionStart: function slideChangeTransitionStart() {
+        $('.case__left').removeClass('animate animate__fadeInLeft');
+      },
+      slideChangeTransitionEnd: function slideChangeTransitionEnd() {
+        $('.case__left').addClass('animate animate__fadeInLeft');
+      }
+    }
+  });
+}
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,41 +1,87 @@
-// $(document).ready(function () {
-//   $('body').autoPadding({
-//     source: $('.js-header'),
-//   });
-//   //removeIf(production)
-//   console.log("document ready");
-//   //endRemoveIf(production)
-// });
+//back to top
 
-
-// fixed header
-
-$(document).ready(function() {
-
-  function stickySidebar() {
-    var scrollDistance = $(document).scrollTop(),
-      headerHeight = $('.header').outerHeight(true),
-      // sidebarHeight = $('aside').outerHeight(true),
-      footerOffsetTop = $('.js-stop-header').offset().top,
-      $header = $('header');
-
-    if( scrollDistance >= headerHeight) {
-      $header.addClass('header_fixed');
-      $header.removeClass('header_hide');
-    } else {
-      $header.removeClass('header_fixed');
-    }
-
-    if ( scrollDistance + headerHeight  >= footerOffsetTop - 100) {
-      $header.removeClass('header_fixed');
-      $header.addClass('header_hide');
-    }
-
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $('.js-to-top').fadeIn();
+  } else {
+    $('.js-to-top').fadeOut();
   }
-  stickySidebar();
-
-  $(document).scroll(function() {
-    stickySidebar();
+});
+$(document).ready(function() {
+  $(".js-to-top").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
   });
 
 });
+
+//openHideMenu
+$(() => {
+  let body = document.querySelector('body')
+  let openElem = document.querySelector('.js-open-hide-menu')
+  let hideMenu = document.querySelector('.hide-menu')
+  let sidebar = document.querySelector('.sidebar')
+  openElem.addEventListener('click', (event) => {
+    event.preventDefault()
+    openElem.classList.toggle('is-active')
+    hideMenu.classList.toggle('is-open')
+    sidebar.classList.toggle('opened-menu')
+    body.classList.toggle('opened-menu')
+  })
+})
+
+// sub menu
+
+const openSubMenu = () => {
+
+  const panelHeading = document.querySelectorAll('.hide-menu__sub'),
+    panelContent = document.querySelector('.sub-menu');
+
+  panelHeading.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      panelHeading.forEach(panelHeading => {
+        panelHeading.classList.remove('is-active')
+      })
+      let target = event.target;
+      elem.classList.add('is-active')
+
+    });
+  });
+};
+
+openSubMenu();
+
+// case slider
+
+let sliderParent = document.getElementsByClassName("case");
+
+for(let i = 0; i < sliderParent.length; i++) {
+
+  let el = sliderParent[i];
+
+  let swiper = el.getElementsByClassName("js-case-swiper")[0];
+  let nx = el.getElementsByClassName("slider-navigation_next")[0];
+  let pr = el.getElementsByClassName("slider-navigation_prev")[0];
+
+  new Swiper (swiper, {
+    speed: 1000,
+    spaceBetween: 75,
+    loop: true,
+    grabCursor: true,
+    // If we need pagination
+    pagination: {
+      el: '.slider-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: nx,
+      prevEl: pr
+    }
+  });
+}
+
+
+
